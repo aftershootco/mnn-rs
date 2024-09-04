30eb42c5a424250b6dbd08441858b691b012fee0
uttarayan21 <email@uttarayan.me>
2024-09-04 14:43:18 +0530

    feat: Added nix flake checks

--- flake.nix
+++ flake.nix
@@ -4,10 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,6 +12,10 @@
     mnn-overlay = {
       url = "github:uttarayan21/mnn-nix-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
@@ -24,6 +25,7 @@
     nixpkgs,
     rust-overlay,
     mnn-overlay,
+    advisory-db,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -40,16 +42,14 @@
         inherit (pkgs) lib;
 
         stableToolchain = pkgs.rust-bin.stable.latest.default;
+        stableToolchainWithLLvmTools = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "llvm-tools"];
+        };
         stableToolchainWithRustAnalyzer = pkgs.rust-bin.stable.latest.default.override {
           extensions = ["rust-src" "rust-analyzer"];
-          # targets = [
-          #   "aarch64-apple-darwin"
-          #   "aarch64-apple-ios"
-          #   "wasm32-unknown-unknown"
-          # ];
         };
-        rustToolchain = pkgs.rust-bin.nightly.latest.default.override {extensions = ["rust-src"];};
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain stableToolchain;
+        craneLibLLvmTools = (crane.mkLib pkgs).overrideToolchain stableToolchainWithLLvmTools;
         src = ./.;
         MNN_SRC = pkgs.fetchFromGitHub {
           owner = "alibaba";
@@ -59,7 +59,7 @@
         };
         commonArgs = {
           inherit src MNN_SRC;
-          pname = "inspect";
+          pname = "mnn";
           # cargoExtraArgs = "--example inspect";
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           # BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.llvmPackages.libclang.lib}/lib/clang/18/include";
@@ -76,14 +76,68 @@
         };
         cargoArtifacts = craneLib.buildPackage commonArgs;
       in {
-        packages = rec {
-          inspect = craneLib.buildPackage (commonArgs
+        checks = {
+          mnn-clippy = craneLib.cargoClippy (commonArgs
             // {
               inherit cargoArtifacts;
-              cargoExtraArgs = "--example inspect";
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
             });
-          default = inspect;
+          mnn-docs = craneLib.cargoDoc (commonArgs // {inherit cargoArtifacts;});
+          mnn-fmt = craneLib.cargoFmt {inherit src;};
+          mnn-toml-fmt = craneLib.taploFmt {
+            src = pkgs.lib.sources.sourceFilesBySuffices src [".toml"];
+          };
+          # Audit dependencies
+          mnn-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          mnn-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on other crate derivations
+          # if you do not want the tests to run twice
+          mnn-nextest = craneLib.cargoNextest (commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            });
+
+          # Ensure that cargo-hakari is up to date
+          mnn-hakari = craneLib.mkCargoDerivation {
+            inherit src;
+            pname = "mnn-hakari";
+            cargoArtifacts = null;
+            doInstallCargoArtifacts = false;
+
+            buildPhaseCargoCommand = ''
+              cargo hakari generate --diff  # workspace-hack Cargo.toml is up-to-date
+              cargo hakari manage-deps --dry-run  # all workspace crates depend on workspace-hack
+              cargo hakari verify
+            '';
+
+            nativeBuildInputs = [
+              pkgs.cargo-hakari
+            ];
+          };
         };
+        packages =
+          rec {
+            mnn = craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;});
+            inspect = craneLib.buildPackage (commonArgs
+              // {
+                inherit cargoArtifacts;
+                pname = "inspect";
+                cargoExtraArgs = "--example inspect";
+              });
+            default = mnn;
+          }
+          // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+            mnn-llvm-cov = craneLibLLvmTools.cargoLlvmCov (commonArgs // {inherit cargoArtifacts;});
+          };
 
         devShells = {
           default = pkgs.mkShell {
