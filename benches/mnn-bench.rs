6ce7bfda5a030afaca64f72d2e3040a9a3ae6b02
uttarayan21 <email@uttarayan.me>
2025-07-17 18:23:08 +0530

    feat: Added benchmarks with metal and coreml

--- benches/mnn-bench.rs
+++ benches/mnn-bench.rs
@@ -30,4 +30,34 @@ mod mnn_realesr_bench_with_ones {
             net.wait(&session);
         });
     }
+
+    #[cfg(feature = "metal")]
+    #[divan::bench]
+    pub fn mnn_realesr_benchmark_metal(bencher: Bencher) {
+        let mut net = Interpreter::from_file("tests/assets/realesr.mnn").unwrap();
+        let mut config = ScheduleConfig::new();
+        config.set_type(ForwardType::Metal);
+        let session = net.create_session(config).unwrap();
+        bencher.bench_local(|| {
+            let mut input = net.input(&session, "data").unwrap();
+            input.fill(1f32);
+            net.run_session(&session).unwrap();
+            net.wait(&session);
+        });
+    }
+
+    #[cfg(feature = "coreml")]
+    #[divan::bench]
+    pub fn mnn_realesr_benchmark_coreml(bencher: Bencher) {
+        let mut net = Interpreter::from_file("tests/assets/realesr.mnn").unwrap();
+        let mut config = ScheduleConfig::new();
+        config.set_type(ForwardType::CoreML);
+        let session = net.create_session(config).unwrap();
+        bencher.bench_local(|| {
+            let mut input = net.input(&session, "data").unwrap();
+            input.fill(1f32);
+            net.run_session(&session).unwrap();
+            net.wait(&session);
+        });
+    }
 }
